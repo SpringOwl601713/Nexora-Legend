381141404a269545a33558ade9e0211ba8e104dd
SpringOwl601713 <72702834+SpringOwl601713@users.noreply.github.com>
2026-09-15 11:57:07 +0200

    Initialize Android bridge before rendering app

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ensureAndroidNexoraBridge } from './android-nexora';
+
+ensureAndroidNexoraBridge();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
